c05ef2afa403f4d678ad8d52833efdaa79ba708b
apollo <apollomacabodbod8@gmail.com>
2024-11-25 09:56:52 +0800

    test

--- src/contact/page.tsx
+++ src/contact/page.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export default function Contact() {
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-orange-400">
+      <div className="flex flex-col px-[2em] bg-orange-400">
         <p className="border border-[#50B498] green">test</p>
         <Button>Destructive</Button>
         <motion.p
