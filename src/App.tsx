3404325df123b801462de1ed7b4d1d9504dfdc16
apollo <apollomacabodbod8@gmail.com>
2025-01-17 06:43:17 +0800

    remove routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./home/page";
-import About from "./about/page";
-import Contact from "./contact/page";
 
 function App() {
   return (
@@ -23,8 +21,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <div>footer</div>
