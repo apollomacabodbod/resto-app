ab2b1aa5c9204275cd18d675c8eb8c6eadf5e803
apollo <apollomacabodbod8@gmail.com>
2025-01-17 06:44:12 +0800

    remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,11 @@ import Home from "./home/page";
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
 
-      <div>footer</div>
 
 
     </Router>
