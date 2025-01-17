5306c3b24a159a31bd068b711acbf67dfcfee3ca
apollo <apollomacabodbod8@gmail.com>
2025-01-17 08:04:25 +0800

    adjust margin top and buttom

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -6,7 +6,7 @@ export default function HomeHeroSection(){
 
   return(<>
 
-    <div className="z-10 flex flex-col my-[15.25em]  max-w-[1200px] lg:mx-auto px-[1.25em]">
+    <div className="z-10 flex flex-col my-[9em]  max-w-[1200px] lg:mx-auto px-[1.25em]">
 
 
       <p className="font-literata text-white text-[7.1875em] not-italic leading-[1em] ">Fresh, fast & delicious!</p>
