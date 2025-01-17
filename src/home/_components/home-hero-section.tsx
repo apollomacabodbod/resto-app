55fc1e721002195b45f97614411b51c45112ccb1
apollo <apollomacabodbod8@gmail.com>
2025-01-17 08:14:44 +0800

    adjust font size in small screens

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -6,19 +6,19 @@ export default function HomeHeroSection(){
 
   return(<>
 
-    <div className="z-10 flex flex-col my-[9em]  max-w-[1200px] lg:mx-auto px-[1.25em]">
+    <div className="z-10 flex flex-col my-[9em]  max-w-[1200px] lg:mx-auto px-[1.25em] border border-[#50B498] green">
 
 
-      <p className="font-literata text-white text-[7.1875em] not-italic leading-[1em] ">Fresh, fast & delicious!</p>
+      <p className="font-literata text-white text-[3.7em] sm:text-[7.1875em] not-italic leading-[1em] border border-[#50B498] green">Fresh, fast & delicious!</p>
 
       <p className="text-white font-inter text-[1.125rem] not-italic font-normal max-w-[33.4375em] mt-[0.9375em]" >Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna.</p>
 
     
-      <div className="flex items-center border border-[#50B498] green mt-[2em] gap-[1.875em]">
+      <div className="sm:flex sm:items-center sm:border border-[#50B498] green mt-[2em] sm:gap-[1.875em]">
 
         <div className="flex flex-col bg-[#E4C620] py-[0.75em] px-[1.5em] rounded-[3.125em]">
 
-          <div className="flex items-center gap-[0.5em] ">
+          <div className="flex items-center justify-center gap-[0.5em] ">
 
             <p className="font-inter text-[1rem] not-italic font-bold ">Book a Table</p>
             <img src={arrow} alt="" />
@@ -29,9 +29,9 @@ export default function HomeHeroSection(){
         </div>
 
 
-        <div className="flex flex-col border border-white py-[0.75em] px-[1.5em] rounded-[3.125em]">
+        <div className="flex flex-col border border-white py-[0.75em] px-[1.5em] mt-[1em] sm:mt-[0em] rounded-[3.125em]">
 
-          <div className="flex items-center gap-[0.5em] ">
+          <div className="flex items-center justify-center gap-[0.5em] ">
 
             <p className="font-inter text-[1rem] not-italic font-bold text-white">The Menus</p>
             <img src={whiteArrow} alt="" />
