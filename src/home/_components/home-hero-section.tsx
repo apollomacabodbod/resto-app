9749ff45e751a56809247d35b54d5c438f2fa51f
apollo <apollomacabodbod8@gmail.com>
2025-01-17 08:03:21 +0800

    added all menus button

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -1,6 +1,6 @@
 
-
-
+import whiteArrow from 'src/assets/home/white-arrow.svg'
+import arrow from "src/assets/home/arrow.svg"
 
 export default function HomeHeroSection(){
 
@@ -13,6 +13,37 @@ export default function HomeHeroSection(){
 
       <p className="text-white font-inter text-[1.125rem] not-italic font-normal max-w-[33.4375em] mt-[0.9375em]" >Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna.</p>
 
+    
+      <div className="flex items-center border border-[#50B498] green mt-[2em] gap-[1.875em]">
+
+        <div className="flex flex-col bg-[#E4C620] py-[0.75em] px-[1.5em] rounded-[3.125em]">
+
+          <div className="flex items-center gap-[0.5em] ">
+
+            <p className="font-inter text-[1rem] not-italic font-bold ">Book a Table</p>
+            <img src={arrow} alt="" />
+
+          </div>
+
+
+        </div>
+
+
+        <div className="flex flex-col border border-white py-[0.75em] px-[1.5em] rounded-[3.125em]">
+
+          <div className="flex items-center gap-[0.5em] ">
+
+            <p className="font-inter text-[1rem] not-italic font-bold text-white">The Menus</p>
+            <img src={whiteArrow} alt="" />
+
+          </div>
+
+
+        </div>
+
+
+      </div>
+    
     </div>
     
     
