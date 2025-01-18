5b3bc09f416858c7efcd95e83148f328437c0ae8
apollo <apollomacabodbod8@gmail.com>
2025-01-18 08:15:27 +0800

    added opening hours

--- src/components/kit/footer.tsx
+++ src/components/kit/footer.tsx
@@ -1,5 +1,5 @@
 
-
+import logo from "src/assets/header/logo.svg"
 
 export default function Footer(){
 
@@ -9,15 +9,105 @@ export default function Footer(){
     <div className="bg-[#28252E]">
 
 
-      <div className="grid grid-cols-4 max-w-[1200px] lg:mx-auto px-[1.25em] pt-[6.25em] border border-[#50B498] green">
+      <div className="grid grid-cols-4 max-w-[1200px] lg:mx-auto gap-[3.75em] px-[1.25em] py-[6.25em] border border-[#50B498] green">
 
         <div className="flex flex-col border border-[#50B498] green">
 
 
-          <p className="text-[#E4C620] ">Know About Resto.</p>
+          <p className="text-[#E4C620] font-literata text-[1.5rem] not-italic font-medium ">Know About Resto.</p>
+
+
+          <p className="text-[#90A3B1] font-inter text-[1rem] not-italic font-normal mt-[1em]">Lorem ipsum dolor sit amet, consec tetur adipi scing elit. Morbi id at mauris dis tincidunt ipsum faucibus ipsum.</p>
+
+
+
+          <img src={logo} alt="" width={120}  className="mt-[2em]"/>
+
+        </div>
+
+
+
+        <div className="flex flex-col border border-[#50B498] green gap-[1em]">
+
+
+          <p className="text-[#E4C620] font-literata text-[1.5rem] not-italic font-medium ">Opening Hours</p>
+
+
+          <p className="text-[#90A3B1] font-inter text-[1rem] not-italic font-normal ">Lorem ipsum dolor sit amet, consectetur</p>
+
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Monday</p>
+            <p className="text-[#90A3B1] ">Closed</p>
+
+
+          </div>
+
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Tuesday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
+
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Wednesday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Thursday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Friday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Saturday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
+
+          <div className="flex items-center justify-between">
+
+            <p className="text-[#FFF4E2] ">Sunday</p>
+            <p className="text-[#90A3B1] ">10 AM - 11 PM</p>
+
+
+          </div>
+
 
 
         </div>
+
+
 
 
 
