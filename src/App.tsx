ccb86d04d77f3f71cff8e9ad1e3d7da4119e9053
apollo <apollomacabodbod8@gmail.com>
2025-01-18 08:02:25 +0800

    added padding top

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./home/page";
 import Header from "./components/kit/header";
 import { LenisProvider } from "./context/smooth-scroll";
 import ScrollToTop from "./components/kit/scroll-top";
+import Footer from "./components/kit/footer";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+
+
+        <Footer/>
 
       </LenisProvider>
     
---
+++ src/components/kit/footer.tsx
@@ -0,0 +1,33 @@
+
+
+
+export default function Footer(){
+
+  return (<>
+
+
+    <div className="bg-[#28252E]">
+
+
+      <div className="grid grid-cols-4 max-w-[1200px] lg:mx-auto px-[1.25em] pt-[6.25em] border border-[#50B498] green">
+
+        <div className="flex flex-col border border-[#50B498] green">
+
+
+          <p className="text-[#E4C620] ">Know About Resto.</p>
+
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+    </div>
+    
+    
+  </>)
+}
