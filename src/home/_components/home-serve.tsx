df38ffae03bd7ce5e17690e845f0517536f32d94
Apollo <apollomacabodbod8@gmail.com>
2025-02-14 20:19:19 +0800

    adjust padding

--- src/home/_components/home-serve.tsx
+++ src/home/_components/home-serve.tsx
@@ -8,7 +8,7 @@ export default function HomeServe(){
   return (<>
 
 
-    <div className="flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em] mt-[5.25em]">
+    <div className="flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em] mt-[5.25em] pb-[23.875em]">
 
       <div className="flex items-center justify-center gap-[0.5em]">
 
@@ -24,6 +24,8 @@ export default function HomeServe(){
       <p className="font-literata text-[3.25rem] text-white text-center not-italic font-medium leading-[1em] ">Fresh & Delicious</p>
 
         
+
+      <p className="text-[#90A3B1] font-inter text-[1rem] not-italic font-normal text-center mt-[1em] ">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Morbi id at mauris dis tincidunt ipsum faucibus ipsum.</p>
         
     </div>
     
