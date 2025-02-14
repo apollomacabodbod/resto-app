353e450d7013c1de60d80b4caa5eb9fca7ee343a
Apollo <apollomacabodbod8@gmail.com>
2025-02-14 15:53:38 +0800

    added smooth transition

--- src/home/_components/about-us.tsx
+++ src/home/_components/about-us.tsx
@@ -2,6 +2,7 @@ import box from 'src/assets/home/about-us-box.svg'
 import redArrow from 'src/assets/about/about-us-arrow.svg'
 import smilingFemale from 'src/assets/about/smiling-female.jpg'
 import diningTable from 'src/assets/about/dining-table.jpg'
+import { motion } from "framer-motion"
 
 export default function HomeAboutUs() {
   return (
@@ -14,25 +15,84 @@ export default function HomeAboutUs() {
 
 
 
-        <div className="flex items-center gap-[0.28125em]">
+        <motion.div className="flex items-center gap-[0.28125em] "
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        >
           <img src={box} alt="" />
           <p className="text-[#90A3B1] font-inter text-[0.875rem]">About</p>
-        </div>
+        </motion.div>
   
-        <p className="font-literata text-[#BB3A12] text-[3.25rem] not-italic font-medium leading-[1em]">
+        <motion.p className="font-literata text-[#BB3A12] text-[3.25rem] not-italic font-medium leading-[1em]"
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        
+        >
             About Us
-        </p>
+        </motion.p>
 
-        <p className='mt-[2em] text-[#28252E] font-inter text-[1rem] not-italic font-normal'>
+        <motion.p className='mt-[2em] text-[#28252E] font-inter text-[1rem] not-italic font-normal'
+        
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        
+        >
             
             
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id at mauris dis tincidunt ipsum faucibus ipsum. At laoreet vivamus ultrices dolor vel nisl.
           <br   />
           <br   />
-           Leo, ultrices enim vel feugiat lectus nisi, phasellus egestas. Nullam tellus aliquam. At laoreet vivamus ultrices dolor vel nisl.</p>
+           Leo, ultrices enim vel feugiat lectus nisi, phasellus egestas. Nullam tellus aliquam. At laoreet vivamus ultrices dolor vel nisl.</motion.p>
         
 
-        <div className='flex items-center mt-[2em] cursor-pointer'>
+        <motion.div className='flex items-center mt-[2em] cursor-pointer'
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        >
 
           <div className='flex flex-col border border-[#BB3A12] py-[0.75em] px-[1.5em] rounded-[3.125em]'>
 
@@ -52,15 +112,48 @@ export default function HomeAboutUs() {
 
              
 
-        </div>
+        </motion.div>
 
 
 
 
-        <img src={smilingFemale} alt="" className='absolute  top-[-13em] md:top-[-14em]  xl:top-[-14em]  left-0   md:left-[10%] xl:left-[30%]   w-[50%] xl:w-[34.75em] h-[15em] lg:h-[20em] md:h-[20em] xl:h-[32.5em] object-top  object-cover z-[-1]' />
+        <motion.img src={smilingFemale} alt="" className='absolute  top-[-13em] md:top-[-14em]  xl:top-[-14em]  left-0   md:left-[10%] xl:left-[30%]   w-[50%] xl:w-[34.75em] h-[15em] lg:h-[20em] md:h-[20em] xl:h-[32.5em] object-top  object-cover z-[-1]' 
+        
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        
+        />
       
       
-        <img src={diningTable} alt="" className='absolute  top-[-13em] md:top-[-14em]  right-0  md:right-[-17.5%]  xl:right-[-137%]   w-[50%] xl:w-[28.5625em] h-[15em] md:h-[20em] xl:h-[38.5625em] object-top  object-cover z-[-1]' />
+        <motion.img src={diningTable} alt="" className='absolute  top-[-13em] md:top-[-14em]  right-0  md:right-[-17.5%]  xl:right-[-137%]   w-[50%] xl:w-[28.5625em] h-[15em] md:h-[20em] xl:h-[38.5625em] object-top  object-cover z-[-1]' 
+        
+        
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        
+        
+        />
 
 
 
