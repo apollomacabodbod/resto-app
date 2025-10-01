b530526fea279e22830aa7fb859e3aaef70b90c2
Apollo <apollomacabodbod8@gmail.com>
2025-10-01 11:41:23 +0800

    added custom select and date

--- src/home/_components/home-serve-wrapper.tsx
+++ src/home/_components/home-serve-wrapper.tsx
@@ -4,11 +4,9 @@ import HomeReservation from "./home-reservation";
 export default function HomeServeWrapper() {
   return (
     <>
-      <div className="grid grid-cols-1  lg:grid-cols-2 max-w-[1200px] lg:mx-auto px-[1.25em]  border border-[#50B498] green">
-        <div className="flex flex-col ">
-          <HomeCards />
-          <HomeReservation />
-        </div>
+      <div className="grid grid-cols-1  lg:grid-cols-2 max-w-[1200px] lg:mx-auto px-[1.25em] gap-[2.4375em] border border-[#50B498] green">
+        <HomeCards />
+        <HomeReservation />
       </div>
     </>
   );
