acd662c1a9eba716a544fd2d9cd15683a7d7209f
Apollo <apollomacabodbod8@gmail.com>
2025-10-21 13:31:08 +0800

    added smooth animation

--- src/reservation/_components/reservation-hero-section.tsx
+++ src/reservation/_components/reservation-hero-section.tsx
@@ -1,17 +1,43 @@
 import { Link } from "react-router-dom";
 import heroSectionArrow from "src/assets/reservation/hero-section-arrow.svg";
+import { motion } from "framer-motion";
 
 export default function ReservationHeroSection() {
   return (
     <>
       <div className="z-10 flex flex-col mt-[7.40625em] mb-[8.71875em] max-w-[1200px] lg:mx-auto px-[1.375em]">
         {/* Title */}
-        <p className="text-[#FFF] font-literata text-[3.375rem]  lg:text-[5.375rem] font-medium ">
+        <motion.p
+          className="text-[#FFF] font-literata text-[3.375rem]  lg:text-[5.375rem] font-medium "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Make a Reservation
-        </p>
+        </motion.p>
 
-        {/* Breadcrumb */}
-        <div className="flex items-center gap-[1.875em]">
+        <motion.div
+          className="flex items-center gap-[1.875em]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           <Link
             to={"/"}
             className="text-[#FFF] font-inter text-[1rem] font-bold"
@@ -23,7 +49,7 @@ export default function ReservationHeroSection() {
           <p className="text-[#E4C620] font-inter text-[1rem] font-bold cursor-pointer">
             Make a Reservation
           </p>
-        </div>
+        </motion.div>
       </div>
     </>
   );
