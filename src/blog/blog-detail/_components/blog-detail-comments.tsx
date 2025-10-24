83dc9b07dc635785f08eafe64b2215069ad2e033
Apollo <apollomacabodbod8@gmail.com>
2025-10-24 09:29:57 +0800

    added smooth animation

--- src/blog/blog-detail/_components/blog-detail-comments.tsx
+++ src/blog/blog-detail/_components/blog-detail-comments.tsx
@@ -1,23 +1,62 @@
 import Rectangle from "src/assets/blog/Rectangle.svg";
-
+import { motion } from "framer-motion";
 export default function BlogDetailComments() {
   return (
     <>
       <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] mt-[6.625em]">
-        <div className="flex items-center gap-[0.5em]">
+        <motion.div
+          className="flex items-center gap-[0.5em]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           <img src={Rectangle} alt="" />
           <p className="text-[#E4C620] font-inter text-[0.875rem] font-semibold uppercase">
             Comments
           </p>
-        </div>
+        </motion.div>
 
-        <p className="text-[#FFF4E2] font-literata text-[3.25rem] font-medium">
+        <motion.p
+          className="text-[#FFF4E2] font-literata text-[3.25rem] font-medium"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Leave a Reply
-        </p>
+        </motion.p>
 
-        <p className="text-[#90A3B1] font-inter text-[1rem] font-normal mt-[0.5em]">
+        <motion.p
+          className="text-[#90A3B1] font-inter text-[1rem] font-normal mt-[0.5em]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Your email address will not be published. Required fields are marked *
-        </p>
+        </motion.p>
       </div>
     </>
   );
